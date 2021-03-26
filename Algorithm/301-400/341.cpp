85d194b2aaab34c2ce07f930ab56ca997b2cf3e6
mtf-tj <80962173+mtf-tj@users.noreply.github.com>
2021-03-26 16:10:04 +0800

    Create 341.cpp

---
+++ Algorithm/301-400/341.cpp
@@ -0,0 +1,55 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+
+class NestedIterator {
+private:
+    stack<pair<vector<NestedInteger>::iterator,vector<NestedInteger>::iterator>> stk;
+public:
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        stk.emplace(nestedList.begin(),nestedList.end());
+    }
+    
+    int next() {
+        return stk.top().first++->getInteger();
+    }
+    
+    bool hasNext() {   
+        while(!stk.empty())
+        {
+            auto &p = stk.top();
+            if(p.first==p.second)
+            {
+                stk.pop();
+                continue;
+            }
+            if(p.first->isInteger())
+            {
+                return true;
+            }
+            auto &l = p.first++->getList();
+            stk.emplace(l.begin(),l.end());
+        }
+        return false;
+    }
+};
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i(nestedList);
+ * while (i.hasNext()) cout << i.next();
+ */
